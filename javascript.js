fb6ccd9dc35d15a26079843160a06f471a4eab2e
Maryam2329 <125149336+Maryam2329@users.noreply.github.com>
2023-05-11 08:40:56 +0200

    turned buttons into objects with button key

--- javascript.js
+++ javascript.js
@@ -2,10 +2,17 @@ const container = document.querySelector('body');
 container.style.display='flex';
 container.style.flexDirection='column';
 
+// test of object creation
+/*
+let objecttest = {
+    symbol: symbol,
+    button : button
+}*/
+
 // BUTTONS CREATION
 
 // all number button creations
-let numberButton =[];
+let numberButtonList =[];
 for (i=1;i<10;i++){
     const button= document.createElement('button');
     button.style.width='70px';
@@ -20,18 +27,22 @@ for (i=1;i<10;i++){
     }
     button.style.color='black';
     button.style.borderRadius='50%';
-    button.style.margin='15px';
-    numberButton.push(button);
-    //container.appendChild(button);
+    button.style.margin='15px'; 
+    let buttonObject = {
+        symbol : i,
+        button : button
+    };
+    numberButtonList.push(buttonObject);
+    //container.appendChild(buttonObject.button);
 }
 /*
-for (i=0;i<numberButton.length;i++){
+for (i=0;i<numberButtonList.length;i++){
     console.log('yo'+i);
-    container.appendChild(numberButton[i]);
+    container.appendChild(numberButtonList[i].button);
 }*/
 
 // defining the operator button
-let operatorButton=[];
+let operatorButtonList=[];
 for (i in operator=['+','-','*','%']){
     const button= document.createElement('button');
     button.style.width='70px';
@@ -43,12 +54,16 @@ for (i in operator=['+','-','*','%']){
     button.style.color='black';
     button.style.borderRadius='50%';
     button.style.margin='15px';
-    operatorButton.push(button);
-    //container.appendChild(button);
+    let buttonObject = {
+        symbol : operator[i].toString(),
+        button : button
+    };
+    operatorButtonList.push(buttonObject);
+    //container.appendChild(buttonObject.button);
 }
 
 // defining button 0 and =
-let zeroAndEqualButton =[];
+let zeroAndEqualButtonList =[];
 for (i in operator=['0','=']){
     const button= document.createElement('button');
     button.style.width='170px';
@@ -64,8 +79,12 @@ for (i in operator=['0','=']){
     button.style.color='black';
     button.style.borderRadius='50%';
     button.style.margin='15px';
-    zeroAndEqualButton.push(button);
-    //container.appendChild(button);
+    let buttonObject = {
+        symbol : operator[i].toString(),
+        button : button
+    };
+    zeroAndEqualButtonList.push(buttonObject);
+    //container.appendChild(buttonObject.button);
 }
 
 // result button
@@ -79,7 +98,11 @@ resultButton.style.backgroundColor='orange';
 resultButton.style.color='black';
 resultButton.style.borderRadius='15%';
 resultButton.style.margin='15px';
-//container.appendChild(resultButton);
+let resultButtonObject = {
+    symbol : 'Here is your operation',
+    button : resultButton
+};
+//container.appendChild(resultButtonObject.button);
 
 // create 3 divs for the three parts (each one is a flex box), 
 //and construct them one by one NB : the middle div might need a flex1
@@ -87,8 +110,8 @@ resultButton.style.margin='15px';
 // SUBCONTAINER 3
 const subcontainer3 = document.createElement('div');
 subcontainer3.style.display='flex';
-subcontainer3.appendChild(resultButton);
-subcontainer3.appendChild(operatorButton[operatorButton.length-1]);
+subcontainer3.appendChild(resultButtonObject.button);
+subcontainer3.appendChild(operatorButtonList[operatorButtonList.length-1].button);
 container.appendChild(subcontainer3);
 
 // SUBCONTAINER 2 and childs
@@ -96,22 +119,22 @@ let subcontainer2array =[];
 for (i=2;i>-1;i--){
     subcontainer2array[i] = document.createElement('div');
     subcontainer2array[i].style.display='flex';
-    let jstart =(numberButton.length-1)-3*(2-i);
-    let jend=(numberButton.length-1)-3*(3-i); // 3 is in fact operatorButton.length-2 but im lazy
+    let jstart =(numberButtonList.length-1)-3*(2-i);
+    let jend=(numberButtonList.length-1)-3*(3-i); // 3 is in fact operatorButtonList.length-2 but im lazy
     for (j=jstart;j>jend;j--){ 
         console.log('test');
-        subcontainer2array[i].appendChild(numberButton[j]);
+        subcontainer2array[i].appendChild(numberButtonList[j].button);
         console.log('testpasse');
     }
-    subcontainer2array[i].appendChild(operatorButton[i]);
+    subcontainer2array[i].appendChild(operatorButtonList[i].button);
     container.appendChild(subcontainer2array[i]);
 }
 
 // SUBCONTAINER 1
 const subcontainer1 = document.createElement('div');
 subcontainer1.style.display='flex';
-for (i=0;i<zeroAndEqualButton.length;i++){
-    subcontainer1.appendChild(zeroAndEqualButton[i]);
+for (i=0;i<zeroAndEqualButtonList.length;i++){
+    subcontainer1.appendChild(zeroAndEqualButtonList[i].button);
 }
 container.appendChild(subcontainer1);
 
@@ -139,8 +162,8 @@ let calculator = function (){
     let Operator='';
     // pour les clicks sur numeros, je sais pas trop comment faire pour l'ordre
     // rajouter un clear button
-    for (i=0;i<operatorButton.length;i++){
-        operatorButton[i].addEventListener('click',()=>{
+    for (i=0;i<operatorButtonList.length;i++){
+        operatorButtonList[i].button.addEventListener('click',()=>{
             // transformer les list de button numeros/operator/etc en listes d'objects, avec 2 clés : le character affiché et l'objet
         });
     }
